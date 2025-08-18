d679b2e7fcc0b76e76e909045ceefeecb259c571
DomiCarr <carrasco.dominique@gmail.com>
2025-08-19 01:06:21 +0200

    :construction: work in progress : Tournament management

--- src/controllers/manage_tournament.py
+++ src/controllers/manage_tournament.py
@@ -26,3 +26,20 @@
 - lorsque le tournoi est fini renseigner la datetime de fin
 - lister les joueurs par ordre de points le gagnant en premier
 """
+
+"""manage_players - Controller for player CLI"""
+
+import click
+
+from views.players_cli import add_player
+
+
+@click.group()
+def mycommands():
+    """CLI for managing players."""
+    pass
+
+
+# Attach commands from views
+mycommands.add_command(add_player)
+mycommands.add_command(list_players)
