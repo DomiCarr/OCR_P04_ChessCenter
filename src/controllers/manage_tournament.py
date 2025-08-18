b49a389c43c21aad6d289e7a96a3de4410848883
DomiCarr <carrasco.dominique@gmail.com>
2025-08-18 22:14:33 +0200

    :construction: work in progress : Tournament management

---
+++ src/controllers/manage_tournament.py
@@ -0,0 +1,28 @@
+"""
+- créer le tournoi: name, location, description
+-tant que le tournoi n'as pas commencé (star_date a blanc)
+    ajouter les joueurs a la liste des joueurs du tournois
+-lancer le tournoi :
+    affecter ongoing_round = 1
+    créer le round 1 : name du round = "name du tournoi + round 1"
+    renseigner la datetime de début du tournoi
+    créer les matchs
+        mélanger la liste des joueurs aléatoirement
+        associer les joueurs par paire
+        mettre un score a zéro a chaque joueur
+        affecter aléatoirement blanc ou noir a chaque joueur
+- tant que le round n'est pas terminé (il reste des matchs avec 2 scores a zero)
+    saisir le résultats d'un match à partir de la liste des matchs
+        modifier le score total des journeurs pour le tournoi
+- incrémenter ne numero du round : ongoing_round =+1
+- tant qu'il reste des rounds à jouer (ongoing_round <= nb_of_rounds)
+    Trier les joueurs par ordre de point
+    Associer les joueurs par paire dans l'ordre
+    mettre un score a zéro a chaque joueur
+    affecter aléatoirement blanc ou noir a chaque joueur
+- tant que le round n'est pas terminé (il reste des matchs avec 2 scores a zero)
+    saisir le résultats d'un match à partir de la liste des matchs
+    modifier le score total des journeurs pour le tournoi
+- lorsque le tournoi est fini renseigner la datetime de fin
+- lister les joueurs par ordre de points le gagnant en premier
+"""
