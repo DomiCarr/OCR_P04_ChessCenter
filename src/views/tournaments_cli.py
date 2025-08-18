9363a9b64236650645050b825b8d69990caba71c
DomiCarr <carrasco.dominique@gmail.com>
2025-08-19 01:07:31 +0200

    :construction: work in progress : Tournament management

--- src/views/tournaments_cli.py
+++ src/views/tournaments_cli.py
@@ -1,15 +1,13 @@
 """Player - Click commands"""
 
 import click
-from models.player import Player
-from models.players import Players
+from models.tournament import Tournament
 
 @click.command()
-@click.option("--id", prompt="National ID")
-@click.option("--first", prompt="First name")
+@click.option("--name", prompt="name")
+@click.option("--location", prompt="location")
 @click.option("--last", prompt="Last name")
-@click.option("--birth", prompt="Birth date")
-def add_player(id, first, last, birth):
+def add_tournament(id, first, last, birth):
     """Add a new player via CLI."""
     players_bdd = Players()
     new_player = Player(id, first, last, birth)
@@ -21,7 +19,7 @@ def add_player(id, first, last, birth):
         click.echo("Player added successfully!")
 
 @click.command()
-def list_players():
+def list_tournaments():
     """List all players sorted by last name then first name."""
     players_bdd = Players()
     all_players = players_bdd.list_players()
